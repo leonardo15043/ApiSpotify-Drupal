c4690252be09a4221eeef7dd1bae9ae65d6be604
leonardo15043 <lahernandez15043@gmail.com>
2018-12-17 22:56:26 -0500

    envio de URL de autorizacion

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -7,11 +7,44 @@
 
 namespace Drupal\spotify\Controller;
 
+use Drupal\spotify\Api\SpotifyWebAPIAuthException;
+use Drupal\spotify\Api\SpotifyWebAPIException;
+use Drupal\spotify\Api\SpotifyWebAPI;
+use Drupal\spotify\Api\Session;
+use Drupal\spotify\Api\Request;
+use Drupal\Core\Routing\TrustedRedirectResponse;
+use Drupal\Core\Url;
+
 class LoginController {
-  public function content() {
-    return array(
-      '#type' => 'markup',
-      '#markup' => t('Pantalla de inicio'),
-    );
+
+  private $access;
+  private $session;
+
+  public function __construct(){
+
+    $this->access = new Session(
+        '7de446abfd16485fb6a769baab33f20c',
+        '11de64c2379e46f2a458b925865c21b9',
+        'http://localhost:8888/drupal/authentication'
+      );
+
+    $this->session = \Drupal::request()->getSession();
+
   }
+
+  public function keyUrl() {
+
+    $scopes = array(
+      'user-read-email',
+      'user-read-private'
+    );
+
+    $authorizeUrl = $this->access->getAuthorizeUrl( array(
+     'scope' => $scopes
+    ));
+
+    return new TrustedRedirectResponse($authorizeUrl);
+
+  }
+
 }
