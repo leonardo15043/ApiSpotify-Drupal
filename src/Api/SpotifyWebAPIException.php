121fb1a7d07438df51742f2142c078bded614783
leonardo15043 <lahernandez15043@gmail.com>
2018-12-17 22:28:19 -0500

    Clases de la API y certificado .pem

---
+++ src/Api/SpotifyWebAPIException.php
@@ -0,0 +1,16 @@
+<?php
+namespace Drupal\spotify\Api;
+
+class SpotifyWebAPIException extends \Exception
+{
+    const TOKEN_EXPIRED = 'The access token expired';
+
+    /**
+     * Returns if the exception was thrown because of an expired token.
+     * @return bool
+     */
+    public function hasExpiredToken()
+    {
+        return $this->getMessage() === self::TOKEN_EXPIRED;
+    }
+}
